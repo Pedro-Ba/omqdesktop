5e8ce00b3312f78d1092f70b788dd7e73961427b
Estia <83793315+Pedro-Ba@users.noreply.github.com>
2023-01-08 10:41:12 -0300

    First playable ver, no userid/secret(need fix)

--- omqdesktop/Form1.Designer.cs
+++ omqdesktop/Form1.Designer.cs
@@ -28,20 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnGetTops = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(353, 134);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 29);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.btnGetTops);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(803, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(785, 426);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(325, 35);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(125, 29);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // btnGetTops
+            // 
+            this.btnGetTops.Location = new System.Drawing.Point(215, 35);
+            this.btnGetTops.Name = "btnGetTops";
+            this.btnGetTops.Size = new System.Drawing.Size(94, 29);
+            this.btnGetTops.TabIndex = 4;
+            this.btnGetTops.Text = "Get Tops";
+            this.btnGetTops.UseVisualStyleBackColor = true;
+            this.btnGetTops.Click += new System.EventHandler(this.btnGetTops_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.BackgroundImage = global::omqdesktop.Properties.Resources.bg;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.lblGuess);
+            this.panel2.Controls.Add(this.lblScore);
+            this.panel2.Controls.Add(this.lblCounter);
+            this.panel2.Controls.Add(this.btnSkip);
+            this.panel2.Controls.Add(this.btnGuess);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.btnPlay);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(788, 457);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(788, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.BackColor = System.Drawing.Color.Transparent;
+            this.lblGuess.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGuess.Location = new System.Drawing.Point(265, 154);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(0, 20);
+            this.lblGuess.TabIndex = 6;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(351, 241);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(80, 20);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score: 0/0";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.BackColor = System.Drawing.Color.Transparent;
+            this.lblCounter.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCounter.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblCounter.Location = new System.Drawing.Point(318, 205);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(152, 20);
+            this.lblCounter.TabIndex = 4;
+            this.lblCounter.Text = "Current Song: 1/100";
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(341, 318);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(94, 29);
+            this.btnSkip.TabIndex = 3;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(578, 362);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(94, 29);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.Location = new System.Drawing.Point(189, 363);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(383, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyUp);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(341, 274);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(94, 29);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play Song";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 653);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private Button btnStart;
+        private Panel panel1;
+        private Button btnGetTops;
+        private Label label1;
+        private TextBox textBox1;
+        private ProgressBar progressBar1;
+        private BindingSource bindingSource1;
+        private Panel panel2;
+        private Button btnPlay;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Button btnSkip;
+        private Button btnGuess;
+        private Label lblScore;
+        private Label lblCounter;
+        private Label lblGuess;
+        private PictureBox pictureBox1;
     }
 }
--- omqdesktop/Form1.cs
+++ omqdesktop/Form1.cs
@@ -1,7 +1,26 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Media;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text.Json.Nodes;
+using NAudio;
+using NAudio.Wave;
+
 namespace omqdesktop
 {
     public partial class Form1 : Form
     {
+        string api_key;
+        HttpClient client = new HttpClient();
+        List<string> titleArtist = new List<string>();
+        List<string> previewUrls = new List<string>();
+        List<string> coverImgs = new List<string>();
+        int currentSong = 0;
+        int currentScore = 0;
+        WaveOutEvent waveoutevent = new WaveOutEvent();
+        int cbwidth = 383;
+        int cbheight = 28;
         public Form1()
         {
             InitializeComponent();
@@ -11,5 +30,260 @@ namespace omqdesktop
         {
 
         }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            var field = typeof(System.Net.Http.Headers.HttpRequestHeaders)
+                .GetField("invalidHeaders", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+              ?? typeof(System.Net.Http.Headers.HttpRequestHeaders)
+                .GetField("s_invalidHeaders", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            if (field != null)
+            {
+                var invalidFields = (HashSet<string>)field.GetValue(null);
+                invalidFields.Remove("Content-Type");
+            }
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var values = new Dictionary<string, string>
+            {
+                { "client_id", "" },
+                { "client_secret", "" },
+                { "grant_type", "client_credentials" },
+                { "scope", "public" }
+            };
+
+            var contentReq = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://osu.ppy.sh/oauth/token", contentReq);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var APIresponseObj = JsonNode.Parse(responseString);
+            api_key = (APIresponseObj["access_token"].ToString());
+            btnStart.Visible = false;
+
+            panel1.Left = 12;
+            panel1.Top = 12;
+            panel1.Visible = true;
+            label2.Text = "This will now get all top beatmaps from the player ID's default game mode, randomize them, and then do a \nlookup to retrieve both an .mp3 preview and the title + artist of every single one of them. \nThis takes a while, so please be patient.";
+
+        }
+
+        private async void btnGetTops_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+            string user_id = textBox1.Text + "/";
+            string type = "best/";
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + api_key);
+
+            string urlUser = "https://osu.ppy.sh/api/v2/users/" + user_id + "scores/" + type + "?limit=100";
+
+            List<string> beatmapIds = new List<string>();
+
+            using (HttpResponseMessage res = await client.GetAsync(urlUser))
+            {
+                using (HttpContent content = res.Content)
+                {
+                    var data = await content.ReadAsStringAsync();
+                    if (data != null)
+                    {
+                        var dataObj = JsonNode.Parse(data);
+                        if (dataObj is JsonArray jArray)
+                        {
+                            foreach (var i in jArray)
+                            {
+                                beatmapIds.Add(i["beatmap"]["id"].ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO Data----------");
+                    }
+                }
+            }
+
+            Console.WriteLine(beatmapIds.Count);
+            //Shuffle beatmaps
+            beatmapIds.Shuffle();
+
+            string urlBeatmap = "https://osu.ppy.sh/api/v2/beatmaps/lookup/";
+
+            for (var i = 0; i < beatmapIds.Count - 1; i++)
+            {
+                progressBar1.Value++;
+                using (HttpResponseMessage res = await client.GetAsync(urlBeatmap + "?id=" + beatmapIds[i]))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        var data = await content.ReadAsStringAsync();
+                        if (data != null)
+                        {
+                            var dataObj = JsonNode.Parse(data);
+                            coverImgs.Add(dataObj["beatmapset"]["covers"]["cover"].ToString());
+                            titleArtist.Add(dataObj["beatmapset"]["artist"].ToString() + " - " + dataObj["beatmapset"]["title"].ToString());
+                            previewUrls.Add(dataObj["beatmapset"]["preview_url"].ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("NO Data----------");
+                        }
+                    }
+                }
+            }
+
+            panel1.Visible = false;
+            updateLbls();
+            panel2.Visible = true;
+            panel2.Top = 0;
+            panel2.Left = 0;
+            panel2.Dock = DockStyle.Fill;
+            comboBox1.DropDownHeight = 200;
+            comboBox1.DroppedDown = true;
+            comboBox1.DropDownStyle = ComboBoxStyle.Simple;
+            comboBox1.Size = new System.Drawing.Size(cbwidth, cbheight);
+        }
+
+        public void PlayMp3FromUrl(string url)
+        {
+            using (Stream ms = new MemoryStream())
+            {
+                using (Stream stream = WebRequest.Create(url)
+                    .GetResponse().GetResponseStream())
+                {
+                    byte[] buffer = new byte[32768];
+                    int read;
+                    while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, read);
+                    }
+                }
+
+                ms.Position = 0;
+                using (WaveStream blockAlignedStream =
+                    new BlockAlignReductionStream(
+                        WaveFormatConversionStream.CreatePcmStream(
+                            new Mp3FileReader(ms))))
+                {                    
+                    waveoutevent.Init(blockAlignedStream);
+                    waveoutevent.Play();
+                    while (waveoutevent.PlaybackState == PlaybackState.Playing)
+                    {
+                    }
+                }
+            }
+        }
+
+        private void startMp3Task()
+        {
+            Task.Factory.StartNew(() => PlayMp3FromUrl("https:" + previewUrls[currentSong]));
+        }
+
+        private void btnPlay_Click_1(object sender, EventArgs e)
+        {
+            startMp3Task();
+        }
+
+        private void comboBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (comboBox1.Text.Length != 0 && !e.KeyCode.Equals(Keys.Up) && !e.KeyCode.Equals(Keys.Down))
+            {
+                int selectionStart = comboBox1.SelectionStart;
+                comboBox1.Items.Clear();
+                for (var i = 0; i < titleArtist.Count - 1; i++)
+                {
+                    var artist = titleArtist[i];
+                    if (artist.ToLower().Contains(comboBox1.Text.ToLower()))
+                    {
+                        comboBox1.Items.Add(artist);
+                    }
+                }
+                comboBox1.SelectionStart = selectionStart;
+                comboBox1.SelectionLength = 0;
+                comboBox1.SelectedIndex = -1;
+                comboBox1.Size = new System.Drawing.Size(cbwidth, cbheight*10);
+            }
+            else
+            {
+                if (comboBox1.Text.Length == 0)
+                {
+                    comboBox1.Items.Clear();
+                    comboBox1.Size = new System.Drawing.Size(cbwidth, cbheight);
+                }
+                else
+                {
+
+                }
+            }
+            if (e.KeyCode.Equals(Keys.Return))
+            {
+                comboBox1.Size = new System.Drawing.Size(cbwidth, cbheight);
+            }
+        }
+
+        private void updateLbls()
+        {
+            lblCounter.Text = "Current Song: " + (currentSong+1).ToString() + "/" + (titleArtist.Count+1).ToString();
+            lblScore.Text = "Score: " + (currentScore).ToString() + "/" + currentSong.ToString();
+            //lblScore;
+
+        }
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            lblGuess.Text = "You skipped!\nThe beatmap was " + titleArtist[currentSong].ToString();
+            lblGuess.ForeColor = Color.Red;
+            goesToNextSong();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string guess = comboBox1.Text;
+            if (guess.ToLower().Equals(titleArtist[currentSong].ToLower()))
+            {
+                lblGuess.Text = "You got it right!\nThe beatmap was " + titleArtist[currentSong].ToString();
+                lblGuess.ForeColor = Color.Green;
+                currentScore++;
+                goesToNextSong();
+            }
+            else
+            {
+                lblGuess.Text = "You got it wrong!\nThe beatmap was " + titleArtist[currentSong].ToString();
+                lblGuess.ForeColor = Color.Red;
+                goesToNextSong();
+            }
+            
+        }
+
+        private void goesToNextSong()
+        {
+            pictureBox1.Load(coverImgs[currentSong]);
+            comboBox1.Text = "";
+            comboBox1.Size = new System.Drawing.Size(cbwidth, cbheight);
+            waveoutevent.Stop();
+            currentSong++;
+            startMp3Task();
+            updateLbls();
+        }
     }
+    public static class ListExtension
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
 }
